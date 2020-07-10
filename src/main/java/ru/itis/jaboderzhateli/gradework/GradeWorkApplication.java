7faf889c315565b4b25fa81884147258409847c8
Hyperb0t <vitalij-groshev@mail.ru>
2020-07-10 20:25:44 +0300

    initial commit

---
+++ src/main/java/ru/itis/jaboderzhateli/gradework/GradeWorkApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.jaboderzhateli.gradework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradeWorkApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ru/itis/jaboderzhateli/gradework/config/WebConfig.java
@@ -0,0 +1,8 @@
+package ru.itis.jaboderzhateli.gradework.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+}
