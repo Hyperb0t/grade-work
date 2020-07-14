c153e07710c660a650308323187c2a48a0dec517
Hyperb0t <vitalij-groshev@mail.ru>
2020-07-14 14:32:20 +0300

    fixed repository test with hsqldb

--- src/test/java/ru/itis/jaboderzhateli/gradework/repositories/StudentCompetenceRepositoryTest.java
+++ src/test/java/ru/itis/jaboderzhateli/gradework/repositories/StudentCompetenceRepositoryTest.java
@@ -1,39 +1,39 @@
 package ru.itis.jaboderzhateli.gradework.repositories;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
-import ru.itis.jaboderzhateli.gradework.config.TestApplicationConfig;
 import ru.itis.jaboderzhateli.gradework.models.Student;
 
-import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
+
+/**
+ * @author Vitaly
+ */
 
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
-@DataJpaTest
 @Slf4j
 public class StudentCompetenceRepositoryTest {
 
+    @Autowired
     private StudentCompetenceRepository studentCompetenceRepository;
+    @Autowired
     private StudentRepository studentRepository;
+    @Autowired
     private CompetenceRepository competenceRepository;
 
-    @BeforeEach
-    void setUp() {
-        ApplicationContext context = new AnnotationConfigApplicationContext(TestApplicationConfig.class);
-        studentCompetenceRepository = context.getBean(StudentCompetenceRepository.class);
-        studentRepository = context.getBean(StudentRepository.class);
-        competenceRepository = context.getBean(CompetenceRepository.class);
-    }
+//    @BeforeEach
+//    void setUp() {
+//        ApplicationContext context = new AnnotationConfigApplicationContext(TestApplicationConfig.class);
+//        studentCompetenceRepository = context.getBean(StudentCompetenceRepository.class);
+//        studentRepository = context.getBean(StudentRepository.class);
+//        competenceRepository = context.getBean(CompetenceRepository.class);
+//    }
 
     @Test
     void studentSaveIdGeneration() {
@@ -42,6 +42,6 @@ public class StudentCompetenceRepositoryTest {
         studentRepository.save(st1);
         studentRepository.save(st2);
         StreamSupport.stream(studentRepository.findAll().spliterator(), false)
-                .forEach(student -> log.info(student.toString()));
+                .forEach(student -> log.info(student.getId() + " " + student.getName()));
     }
 }
