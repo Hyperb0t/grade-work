c056b15307f3571c045c9186c6cc54537374c5d4
Zagir Dingizbaev <Zagir.dingizbaev@yandex.ru>
2020-07-14 12:00:44 +0300

    findByName method added to InstituteRepository

--- src/main/java/ru/itis/jaboderzhateli/gradework/repositories/InstituteRepository.java
+++ src/main/java/ru/itis/jaboderzhateli/gradework/repositories/InstituteRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.itis.jaboderzhateli.gradework.models.Institute;
 
+import java.util.Optional;
+
 @Repository
 public interface InstituteRepository extends CrudRepository<Institute, Long> {
+
+    Optional<Institute> findByName(String name);
+
 }
