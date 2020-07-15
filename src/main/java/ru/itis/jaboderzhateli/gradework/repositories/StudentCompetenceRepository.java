1e3fc0dad28ffab2d55698112b97ecf322313896
Hyperb0t <vitalij-groshev@mail.ru>
2020-07-15 20:35:13 +0300

    fix StudentCompetenceRepo to JpaRepo

--- src/main/java/ru/itis/jaboderzhateli/gradework/repositories/StudentCompetenceRepository.java
+++ src/main/java/ru/itis/jaboderzhateli/gradework/repositories/StudentCompetenceRepository.java
@@ -1,6 +1,6 @@
 package ru.itis.jaboderzhateli.gradework.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.itis.jaboderzhateli.gradework.models.StudentCompetence;
 import ru.itis.jaboderzhateli.gradework.models.StudentCompetenceId;
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface StudentCompetenceRepository extends CrudRepository<StudentCompetence, StudentCompetenceId> {
-    public List<StudentCompetence> findAllByConfirmedIsAndCompetence_Teachers(Boolean confirmed, Teacher teacher);
+public interface StudentCompetenceRepository extends JpaRepository<StudentCompetence, StudentCompetenceId> {
+    List<StudentCompetence> findAllByConfirmedIsAndCompetence_Teachers(Boolean confirmed, Teacher teacher);
     Optional<StudentCompetence> findByStudentIdAndCompetenceId(Long studentId, Long competenceId);
 }
