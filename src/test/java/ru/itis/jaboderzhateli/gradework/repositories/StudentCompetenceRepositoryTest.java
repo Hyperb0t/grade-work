a948ce2bba91ac8690fc0e81860d36706b017ff8
Hyperb0t <vitalij-groshev@mail.ru>
2020-07-22 15:04:04 +0300

    fix schema.sql for tests

--- src/test/java/ru/itis/jaboderzhateli/gradework/repositories/StudentCompetenceRepositoryTest.java
+++ src/test/java/ru/itis/jaboderzhateli/gradework/repositories/StudentCompetenceRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.itis.jaboderzhateli.gradework.models.Student;
 
